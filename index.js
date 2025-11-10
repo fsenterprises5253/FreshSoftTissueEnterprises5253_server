502d0ac9a39f55883919ff8047dcbed73305b83d
Hussain <hussain.f.rang29@gmail.com>
2025-11-10 22:37:25 +0530

    move server to src/server

--- index.js
+++ index.js
@@ -43,7 +43,6 @@ app.post("/api/login", async (req, res) => {
   }
 });
 
-// ✅ Dynamic port support for Render
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
